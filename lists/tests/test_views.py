950a0c612b6537d91e37f7cfe6df1a8c353c4f68
Von V Van <simplevance@gmail.com>
2015-11-16 16:51:09 +0800

    FIRST COMMIT AFTER BACK UP CHAPTER 11 use new form in_home_page, simplify tests. NB breaks stuff

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -5,18 +5,19 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
+from lists.forms import ItemForm
 
 class HomePageTest(TestCase):
 
-	def test_root_url_resolves_to_home_page_view(self):
-		found = resolve('/')
-		self.assertEqual(found.func, home_page)
+	maxDiff = None
 
-	def test_home_page_returns_correct_html(self):
-		request = HttpRequest()
-		response = home_page(request)
-		expected_html = render_to_string('home.html')
-		self.assertEqual(response.content.decode(), expected_html)
+	def test_home_page_renders_home_template(self):
+		response = self.client.get('/')
+		self.assertTemplateUsed(response, 'home.html') #
+	
+	def test_home_page_uses_item_form(self):
+		response = self.client.get('/')
+		self.assertIsInstance(response.context['form'], ItemForm)
 
 class ListViewTest(TestCase):
 
@@ -47,6 +48,38 @@ class ListViewTest(TestCase):
 		response = self.client.get('/lists/%d/' % (correct_list.id,))
 		self.assertEqual(response.context['list'], correct_list)
 
+	def test_can_save_a_POST_request_to_an_existing_list(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+		self.client.post(
+		'/lists/%d/' % (correct_list.id,),
+		data={'item_text': 'A new item for an existing list'}
+		)
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new item for an existing list')
+		self.assertEqual(new_item.list, correct_list)
+	
+	def test_POST_redirects_to_list_view(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+		response = self.client.post(
+		'/lists/%d/' % (correct_list.id,),
+		data={'item_text': 'A new item for an existing list'}
+		)
+		self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+
+	def test_validation_errors_end_up_on_lists_page(self):
+		list_ = List.objects.create()
+		response = self.client.post(
+			'/lists/%d/' % (list_.id,),
+			data={'item_text': ''}
+		)
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'list.html')
+		expected_error = escape("You can't have an empty list item")
+		self.assertContains(response, expected_error)
+
 class NewListTest(TestCase):
 
 	def test_saving_a_POST_request(self):
@@ -54,7 +87,7 @@ class NewListTest(TestCase):
 			'/lists/new',
 			data={'item_text': 'A new list item'}
 		)
-		self.assertEqual(Item.objects.count(), 0)
+		self.assertEqual(Item.objects.count(), 1)
 		new_item = Item.objects.first()
 		self.assertEqual(new_item.text, 'A new list item')
 
@@ -77,29 +110,3 @@ class NewListTest(TestCase):
 		self.client.post('/lists/new', data ={'item_text': ''})
 		self.assertEqual(List.objects.count(), 0)
 		self.assertEqual(Item.objects.count(), 0)
-
-class NewItemTest(TestCase):
-
-	def test_can_save_a_POST_request_to_an_existing_list(self):
-		other_list = List.objects.create()
-		correct_list = List.objects.create()
-
-		self.client.post(
-			'/lists/%d/add_item' % (correct_list.id),
-			data={'item_text': 'A new item for an existing list'}
-		)
-
-		self.assertEqual(Item.objects.count(), 1)
-		new_item = Item.objects.first()
-		self.assertEqual(new_item.text, 'A new item for an existing list')
-		self.assertEqual(new_item.list, correct_list)
-
-	def test_redirects_to_list_view(self):
-		other_list = List.objects.create()
-		correct_list = List.objects.create()
-		response = self.client.post(
-			'/lists/%d/add_item' % (correct_list.id),
-			data={'item_text': 'A new item for an existing list'}
-		)
-
-		self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
--- lists/views.py
+++ lists/views.py
@@ -1,18 +1,27 @@
 from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render	
 from lists.models import Item, List
+from lists.forms import ItemForm
 
 # Create your views here.
 def home_page(request):
-	if request.method == 'POST':
-		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/lists/the-only-list-in-the-world/')
-	return render(request, 'home.html')
+	return render(request, 'home.html', {'form': ItemForm()})
 
 def view_list(request, list_id):
 	list_ = List.objects.get(id=list_id)
-	items = Item.objects.filter(list=list_)
-	return render(request, 'list.html', {'list': list_})
+	error = None
+	
+	if request.method == 'POST':
+		try:
+			item = Item(text=request.POST['item_text'], list=list_)
+			item.full_clean()
+			item.save()
+			return redirect(list_)
+		except ValidationError:
+			error = "You can't have an empty list item"
+	
+	return render(request, 'list.html', {'list': list_, 'error': error})
+
 
 def new_list(request):
 	list_ = List.objects.create()
@@ -24,9 +33,4 @@ def new_list(request):
 		list_.delete()
 		error = "You can't have an empty list item"
 		return render(request, 'home.html', {"error": error})
-	return redirect('/lists/%d/' % (list_.id))
-
-def add_item(request, list_id):
-	list_ = List.objects.get(id=list_id)
-	Item.objects.create(text=request.POST['item_text'], list=list_)
-	return redirect('/lists/%d/' % (list_.id,))
+	return redirect(list_)
