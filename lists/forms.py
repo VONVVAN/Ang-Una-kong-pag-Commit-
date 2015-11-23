5a58dcabeaa568e4999cc9530bb5ffa75100c192
Von V Van <simplevance@gmail.com>
2015-11-23 13:59:47 +0800

    END OF CHAPTER 13 WITH JAVASCRIPT git status! HORAAAAYYYYYY

--- lists/forms.py
+++ lists/forms.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django import forms
 from lists.models import Item
 
@@ -28,10 +29,14 @@ class ItemForm(forms.models.ModelForm):
 class ExistingListItemForm(ItemForm):
 	def __init__(self, for_list, *args, **kwargs):
 		super().__init__(*args, **kwargs)
+		self.instance.list = for_list
 
-	def validate_unique(sel):
+	def validate_unique(self):
 		try:
 			self.instance.validate_unique()
 		except ValidationError as e:
 			e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
 			self._update_errors(e)
+
+	def save(self):
+		return forms.models.ModelForm.save(self)
